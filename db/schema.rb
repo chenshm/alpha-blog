cb118e947badf07afa63987aebe0e5652f6e0500
chenshm <844650898@qq.com>
2022-12-26 14:56:19 -0500

    add secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_26_163743) do
+ActiveRecord::Schema.define(version: 2022_12_26_194650) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_12_26_163743) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
